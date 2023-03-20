8872b77f049a39e4c5004e3905448db40a628222
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-03-20 16:24:32 +0530

    Giving TLE on test case 13

---
+++ C. Sereja and Brackets/practice.cpp
@@ -0,0 +1,109 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+class info {
+
+public:
+    int open,close,full;
+
+public:
+    info(int _open,int _close,int _full){
+        open = _open;
+        close = _close;
+        full = _full;
+
+    }
+
+    info(){
+        open = 0;
+        close = 0;
+        full = 0;
+
+    }
+
+};
+
+
+info merge(info left,info right){
+    info ans = info(0,0,0);
+
+    ans.full = left.full+right.full+min(left.open,right.close);
+    ans.open  = left.open+right.open-min(left.open,right.close);
+    ans.close = left.close+right.close-min(left.open,right.close);
+
+    return ans;
+
+}
+
+void build(int ind,int low,int high,string s,info seg[]){
+    if(low==high){
+        seg[ind].open = s[low]=='(';
+        seg[ind].close = s[low]==')';
+        seg[ind].full = 0;
+
+        return;
+    }
+    int mid = (low+high)/2;
+
+    build(2*ind+1,low,mid,s,seg);
+    build(2*ind+2,mid+1,high,s,seg);
+
+    seg[ind] = merge(seg[2*ind+1],seg[2*ind+2]);
+
+}
+
+info query(int ind,int low,int high,int l,int r,info seg[]){
+    if(r<low || high<l)
+        return info();
+    if(low>=l && high<=r)
+        return seg[ind];
+
+    int mid = (low+high)/2;
+    info left  = query(2*ind+1,low,mid,l,r,seg);
+    info right = query(2*ind+2,mid+1,high,l,r,seg);
+
+    return merge(left,right);
+
+}
+
+void solve(){
+    string s;
+    cin>>s;
+    int n = s.length();
+    info *seg = new info[4*n];
+
+    build(0,0,n-1,s,seg);
+
+    int q;
+    cin>>q;
+
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        l--,r--;
+        info  ans = query(0,0,n-1,l,r,seg);
+        cout<<ans.full*2<<endl;
+    }
+   // cout<<s<<endl;
+}
+
+int main() {
+
+  
+
+  
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif 
+
+     solve();
+    
+
+
+return 0;
+}
